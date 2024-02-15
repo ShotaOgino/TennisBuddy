5ff1ea68b6f52bcc634b3798b07f60985e131019
ShotaOgino <ogishou.shouta@gmail.com>
2024-02-15 22:58:32 +0900

    reduce the unnecessary codes

--- Flutter/my_web_app/lib/upload_file_view.dart
+++ Flutter/my_web_app/lib/upload_file_view.dart
@@ -1,28 +1,23 @@
 import 'dart:io';
 import 'dart:math';
 
-import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:path/path.dart' as path;
 import 'package:upload_with_presigned_url/network_service.dart';
 import 'package:upload_with_presigned_url/upload_data_request_model.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 's3_class_path.dart';
-
 import 'package:minio_new/minio.dart';
-
 import 'package:logger/logger.dart';
 
 final minio = Minio(
-  endPoint: 's3-ap-northeast-1.amazonaws.com',
-  region: 'ap-northeast-1',
-  accessKey: 'AKIA6GBMFUPG6RC5UAIH',
-  secretKey: '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',
+  endPoint: dotenv.env['END_POINT']!,
+  region: dotenv.env['REGION']!, 
+  accessKey: dotenv.env['ACCESS_KEY']!,
+  secretKey:dotenv.env['SECRET_KEY']!,
 );
 
 var logger = Logger();
@@ -43,27 +38,10 @@ class _UploadFileViewState extends State<UploadFileView> {
   @override
   void initState() {
     super.initState();
-    /* loadUploadData(); */
-    dotenv.load(); // 環境変数を読み込む
+    dotenv.load(); // load the environment file
   }
 
-  /* Future<void> loadUploadData() async {
-    final jsonString =
-        await rootBundle.loadString('../assets/config/upload_config.json');
-    final Map<String, dynamic> jsonMap = json.decode(jsonString);
-    setState(() {
-      uploadData = UploadData(
-        url: jsonMap['url'],
-        fields: Map<String, String>.from(jsonMap['fields']),
-      );
-    });
-  } */
-
   Future<void> uploadSelectedFile() async {
-    /* if (uploadData == null || selectedFile.value == null) {
-      logger.d("hello");
-      return;
-    } */
     if (selectedFile.value == null) {
       return;
     }
@@ -72,8 +50,6 @@ class _UploadFileViewState extends State<UploadFileView> {
         await getFileBytesAndName(selectedFile.value);
 
     try {
-      //final minio = S3.instance.getMinio();
-      //final bucketName = dotenv.env['BUCKET_NAME']!;
 
       // List<int>をStream<Uint8List>に変換
       /* final stream = Stream<Uint8List> imageBytes = Stream.value(bucketName.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
@@ -87,8 +63,8 @@ class _UploadFileViewState extends State<UploadFileView> {
         /* Stream<Uint8List>.value(Uint8List(1024)),
         onProgress: (bytes) => logger.d('$bytes uploaded'), */
         stream,
-      size:selectedFileBytes.length, // ストリームの長さを指定
-      onProgress: (bytes) => logger.d('$bytes uploaded'),
+        size: selectedFileBytes.length, // ストリームの長さを指定
+        onProgress: (bytes) => logger.d('$bytes uploaded'),
       );
 
       // アップロード成功時の処理（例：成功メッセージの表示など）
@@ -124,9 +100,6 @@ class _UploadFileViewState extends State<UploadFileView> {
 
     if (result != null && result.files.isNotEmpty) {
       selectedFile.value = result.files.single;
-
-      ///Sample data for uploadData, replace with your own data
-      //uploadData = result;
     }
   }
 
